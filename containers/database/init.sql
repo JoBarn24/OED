0136ac77d264b4423dcb235bf2412eb903775d87
Jody Barnett <jobarnett@csumb.edu>
2026-03-26 12:27:06 -0700

    Add TimescaleDB setup, SQL scripts, and Docker updates

--- containers/database/init.sql
+++ containers/database/init.sql
@@ -8,10 +8,4 @@ CREATE DATABASE oed WITH OWNER oed;
 DROP DATABASE IF EXISTS oed_testing;
 CREATE DATABASE oed_testing WITH OWNER oed;
 
-\connect oed
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-
-\connect oed_testing
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-
 ALTER ROLE oed SUPERUSER;
